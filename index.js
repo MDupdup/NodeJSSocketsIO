9a91bbb87b6b3aec230dc3864ef3192a0d664955
mdupdup <malo.dupont@ynov.com>
2018-11-13 15:27:06 +0100

    Init

---
+++ index.js
@@ -0,0 +1,38 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+let users = {};
+
+io.on('connection', socket => {
+    console.log('user connected: ', socket.id);
+    users[socket.id] = 'anon';
+
+    socket.on('loaded', (data) => {
+        console.log('data from client :', data);
+    });
+
+    socket.on('message', (data) => {
+        socket.broadcast.emit('message', data);
+    });
+
+    socket.on('username', (nick) => {
+        users[socket.id] = nick;
+        io.emit('users', users);
+    });
+
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+    })
+
+    io.emit('users', users);
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+http.listen(3000, () => {
+    console.log('Server is listening on http://localhost:3000');
+});
